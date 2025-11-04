7bfe3bb22792d17fc59e7a5bc602a00dd27cde31
ankitadas100 <ankitadas2308@gmail.com>
2025-11-04 21:24:45 +0530

    day6

---
+++ day6.py
@@ -0,0 +1,13 @@
+marks= int(input("enter the marks:"))
+if(90<=100):
+    print("grade:Ex")
+elif(80<=90):
+    print("grade:A")
+elif(70<=80):
+    print("grade:A")
+elif(60<=70):
+    print("grade:A")
+elif(50<=60):
+    print("grade:A")
+elif(50<=0):
+    print("grade:f")
